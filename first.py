b5f60e8742ce7b40f8d932eb8f024eec97de6c81
abbes maryam <mariam.abbes@enis.tn>
2026-04-25 22:58:38 +0100

    Ajout du  calcul initial

--- first.py
+++ first.py
@@ -1,3 +1,7 @@
-n=5*5
-print (n)
-print (n/5)
+
+def operation_principale(a, b):
+    # Ligne à modifier pour créer le conflit
+    print("Calcul en cours...")
+    return a + b
+
+print(operation_principale(10, 5))
